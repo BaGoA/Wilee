971904d8e9f1e456654d424d80f4b796b4fb46a1
Bastian Gonzalez Acevedo <bastiangonzalezacevedo@gmail.com>
2023-02-19 18:24:42 +0100

    Implement representation of 2-dimensionals point

--- wilee_core/src/point2.rs
+++ wilee_core/src/point2.rs
@@ -16,6 +16,34 @@ You should have received a copy of the GNU Lesser General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-mod cmp_approx;
-mod point2;
-mod sampled_fn;
+/// Representation of  2-dimensional point
+/// x and y correspond to first and second coordinates
+#[derive(Default)]
+pub struct Point2<T> {
+    pub x: T,
+    pub y: T,
+}
+
+impl<T> Point2<T> {
+    /// Create a 2-dimensionals point from two values
+    fn new(x: T, y: T) -> Self {
+        return Self { x, y };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::cmp_approx::*;
+    use super::*;
+
+    #[test]
+    fn test_point2_constructor() {
+        let x: f64 = 1.3;
+        let y: f64 = 4.3;
+        let point: Point2<f64> = Point2::new(x, y);
+
+        let precision: f64 = 0.01;
+        assert!(point.x.approx(&x, precision));
+        assert!(point.y.approx(&y, precision));
+    }
+}
